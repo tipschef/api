2b52208ef7aeaaa3c9bfacef673f7ada1db87559
nisole.bastien@outlook.fr <nisole.bastien@outlook.fr>
2021-05-16 14:33:03 +0200

    [fix/build] removed socket test

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 async def validation_exception_handler(_: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+        content=jsonable_encoder({'detail': exc.errors(), 'body': exc.body}),
     )
 
 
