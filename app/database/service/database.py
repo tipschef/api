efd6c6da9e6641687af22579c6d28a9d06cf8f1c
nisole.bastien@outlook.fr <nisole.bastien@outlook.fr>
2021-05-28 19:21:05 +0200

    Fixed database

--- app/database/service/database.py
+++ app/database/service/database.py
@@ -9,13 +9,8 @@ from app.common.service.secret_manager_service import get_secret_manager_service
 secret_manager_service = get_secret_manager_service()
 secret_content = secret_manager_service.get_secret_json()
 
-localhost = os.getenv('LOCALHOST', False)
-if localhost:
-    DATABASE_NAME = 'tipschef'
-    SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{secret_content.get("mysql_account")}:{secret_content.get("mysql_password")}@{secret_content.get("mysql_hostname")}/{DATABASE_NAME}'
-else:
-    DATABASE_NAME = 'tipschef'
-    SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{secret_content.get("mysql_account")}:{secret_content.get("mysql_password")}@{secret_content.get("mysql_hostname")}/{DATABASE_NAME}?unix_socket=/cloudsql/{secret_content.get("mysql_connection_name")}'
+DATABASE_NAME = 'tipschef'
+SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{secret_content.get("mysql_account")}:{secret_content.get("mysql_password")}@{secret_content.get("mysql_hostname")}/{DATABASE_NAME}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
