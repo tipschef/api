91f603b8c3d20b91cbcb9de5aa67cb6362ac8203
nisole.bastien@outlook.fr <nisole.bastien@outlook.fr>
2021-05-31 23:57:17 +0200

    [feature/file] Added bucket manager service

---
+++ app/common/service/bucket_manager_service.py
@@ -0,0 +1,34 @@
+import os
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from fastapi import UploadFile
+from google.cloud import storage
+from google.cloud.storage import Client
+from typing.io import IO
+
+
+@dataclass
+class BucketManagerService:
+    env: str
+    bucket_name: str = field(init=False)
+    bucket_client: Client = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.bucket_name = f'tipschef-{self.env}-recipes'
+        self.bucket_client = storage.Client()
+
+    def save_file(self, path: str, content: Optional[IO]) -> str:
+        bucket = self.bucket_client.get_bucket(self.bucket_name)
+        blob = bucket.blob(path)
+        with content as file_content:
+            blob.upload_from_file(file_content)
+            blob.make_public()
+            return blob.public_url
+
+    def save_files(self, paths: List[UploadFile]) -> List[str]:
+        return [self.save_file(filepath.filename, filepath.file) for filepath in paths]
+
+
+def get_bucket_manager_service() -> BucketManagerService:
+    return BucketManagerService(os.getenv('PROJECT_ENV'))
