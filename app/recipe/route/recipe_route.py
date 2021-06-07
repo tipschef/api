2766381b60d67d4037800e4d1d1a043a04b91260
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2021-06-07 18:41:38 +0200

    Linter

--- app/recipe/route/recipe_route.py
+++ app/recipe/route/recipe_route.py
@@ -37,8 +37,7 @@ async def create_recipe(recipe: RecipeBaseSchema, database: Session = Depends(ge
 @router.post('/{recipe_id}/media', response_model=List[MediaSchema], tags=['recipes'])
 async def add_medias_to_recipe(recipe_id: int, files: List[UploadFile] = File(...),
                                database: Session = Depends(get_database),
-                               current_user: UserSchema = Depends(UserService.get_current_active_user)) -> List[
-    MediaSchema]:
+                               current_user: UserSchema = Depends(UserService.get_current_active_user)) -> List[MediaSchema]:
     try:
         medias = RecipeService.add_media_to_recipe(database, current_user.id, recipe_id, files)
         return medias
@@ -50,19 +49,17 @@ async def add_medias_to_recipe(recipe_id: int, files: List[UploadFile] = File(..
 
 @router.get('/me', response_model=List[RecipeResponseSchema], tags=['recipes'])
 async def get_my_recipe(database: Session = Depends(get_database),
-                        current_user: UserSchema = Depends(UserService.get_current_active_user)) -> List[
-    RecipeResponseSchema]:
+                        current_user: UserSchema = Depends(UserService.get_current_active_user)) -> List[RecipeResponseSchema]:
     try:
         recipe_list = RecipeService.get_all_recipe_for_specific_user(database, current_user.id)
         return recipe_list
     except Exception as exception:
         print(exception)
-        raise HTTPException(status_code=500, detail='Server exception')
+        raise HTTPException(status_code=500, detail='Server expception')
 
 
 @router.post('/init', response_model=dict, tags=['recipes', 'admin'])
-async def init_database(database: Session = Depends(get_database),
-                        current_user: UserSchema = Depends(UserService.get_current_active_user)) -> dict:
+async def init_database(database: Session = Depends(get_database)) -> dict:
     # TODO : Vérifier que l'utilsateur courant est un administrateur
     try:
         init_data(database)
@@ -74,8 +71,7 @@ async def init_database(database: Session = Depends(get_database),
 
 @router.get('/wall', response_model=List[RecipeResponseSchema], tags=['recipes', 'wall'])
 async def get_my_wall(database: Session = Depends(get_database),
-                      current_user: UserSchema = Depends(UserService.get_current_active_user)) -> List[
-    RecipeResponseSchema]:
+                      current_user: UserSchema = Depends(UserService.get_current_active_user)) -> List[RecipeResponseSchema]:
     try:
         return RecipeService.get_my_wall(database, current_user)
     except Exception as exception:
