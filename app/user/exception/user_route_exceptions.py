a8422b45114239d0a96dc77b9d2f7b30019cd204
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2021-07-02 22:59:49 +0200

    Linter

--- app/user/exception/user_route_exceptions.py
+++ app/user/exception/user_route_exceptions.py
@@ -66,4 +66,4 @@ class EmailAlreadyExistsException(Exception):
 class UserNotPartnerException(Exception):
 
     def __str__(self):
-        return f"User used for this request is not partner."
+        return "User used for this request is not partner."
